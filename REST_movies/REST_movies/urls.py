772e48d726159dbf20861bba1aade8300e2d9e0e
Marta Żaryn <marta@Martas-MacBook-Pro.local>
2017-04-03 14:20:05 +0200

    Rozbudowanie modelu

--- REST_movies/REST_movies/urls.py
+++ REST_movies/REST_movies/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from movies.views import MoviesView
+from movies.views import MovieView, MoviesView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^movies/', MoviesView.as_view())
+    url(r'^admin/$', admin.site.urls),
+    url(r'^movies/$', MoviesView.as_view()),
+    url(r'^movie/(?P<id>(\d)+)/$', MovieView.as_view()),
 ]
--- REST_movies/movies/views.py
+++ REST_movies/movies/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Movie
 
@@ -22,3 +23,29 @@ class MoviesView(APIView):
 		serializer = MovieSerializer(movies, many=True, context={'request':request})
 		return Response(serializer.data)
 
+class MovieView(APIView):
+
+	def get_object(self, pk):
+		try:
+			return Movie.objects.get(pk=pk)
+		except Movie.DoesNotExist:
+			raise Http404
+
+	def get(self, request, id, format=None):
+		movie = self.get_object(id)
+		serializer = MovieSerializer(movie, context={'request':request})
+		return Response(serializer.data)
+
+	def delete(self, request, id, format=None):
+		movie = self.get_object(id)
+		movie.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
+	def put(self, request, id, format=None):
+		movie = self.get_object(id)
+		serializer = MovieSerializer(movie, context={'request':request})
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data
+
+		return Response(serializer.errors, status=statu.HTTP_400_BAD_REQUEST)
